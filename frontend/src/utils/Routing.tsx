05781e0a56f2acaa9141ba87ac1a976090cf2203
Akash2Patil <akashpatil1321999@gmail.com>
2025-09-23 20:47:12 +0530

    git push origin main

--- frontend/src/utils/Routing.tsx
+++ frontend/src/utils/Routing.tsx
@@ -3,6 +3,7 @@ import Signup from '../pages/Signup'
 import Signin from '../pages/Signin'
 import Blog from '../pages/Blog'
 import Blogs from '../pages/Blogs'
+import Published from '../pages/Published'
 
 const Routing = () => {
   return (
@@ -12,6 +13,7 @@ const Routing = () => {
         <Route path='/signin' element={<Signin/>}/>
         <Route path='/blog/:id' element={<Blog/>}/>
         <Route path='/blogs' element={<Blogs/>}/>
+        <Route path='/published' element={<Published/>}/>
     </Routes>
     
   )
