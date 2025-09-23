6f9518a92e24a5ee6c4d893585922bc7977e5757
Akash2Patil <akashpatil1321999@gmail.com>
2025-09-23 20:48:01 +0530

    the appbar updated

--- frontend/src/components/Appbar.tsx
+++ frontend/src/components/Appbar.tsx
@@ -1,10 +1,12 @@
+import { Link } from "react-router-dom"
 import { Avatar } from "./BlogCard"
 
 const Appbar = () => {
   return (
     <div className='border-b-1 border-zinc-500 flex justify-between items-center px-10 py-3'>
-        <div className="text-lg font-bold">Medium</div>
-        <div>
+        <Link to='/blogs' className="text-lg font-bold">Medium</Link>
+        <div className="flex items-center gap-2">
+          <Link to="/published" className="bg-zinc-500 p-3  text-white rounded-full">Write a Blog</Link>
             <Avatar name="Akash patil"/>
         </div>
     </div>
