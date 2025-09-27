b653824ee2b6992f8155858a9aa3dcfec94455f9
Akash2Patil <akashpatil1321999@gmail.com>
2025-09-27 22:37:48 +0530

    doc formatted

--- frontend/src/components/Appbar.tsx
+++ frontend/src/components/Appbar.tsx
@@ -4,11 +4,11 @@ import { Avatar } from "./BlogCard"
 const Appbar = () => {
   return (
     <div className='border-b-1 border-zinc-500 flex justify-between items-center px-10 py-3'>
-        <Link to='/blogs' className="text-lg font-bold">Medium</Link>
-        <div className="flex items-center gap-2">
-          <Link to="/published" className="bg-zinc-500 p-3  text-white rounded-full">Write a Blog</Link>
-            <Avatar name="Akash patil"/>
-        </div>
+      <Link to='/blogs' className="text-lg font-bold">Medium</Link>
+      <div className="flex items-center gap-2">
+        <Link to="/published" className="bg-zinc-500 p-3  text-white rounded-full">Write a Blog</Link>
+        <Avatar name="Akash patil" />
+      </div>
     </div>
   )
 }
